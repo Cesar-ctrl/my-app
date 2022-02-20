fae65636732056a43ab7ecd9d16b61d000ceaa6c
César <80521851+Cesar-ctrl@users.noreply.github.com>
2022-02-20 21:46:07 +0100

    /

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -26,7 +26,7 @@ const Works = () => {
         <div className="card-group">
             <div className="card">
                 <style>{css}</style>
-                <img src={process.env.PUBLIC_URL + 'img/home.PNG'} alt="home" />
+                <img src={process.env.PUBLIC_URL + '/img/home.PNG'} alt="home" />
                 <div className="card-body">
                     <p className="card-text">To increase my skills as a Full-Stack developer, I developed a social network using PHP, Laravel, and
 Javascript. The project is deployed, you can upload images, comment on images, like images, view user
@@ -36,7 +36,7 @@ profiles and delete or edit comments on your own posts.</p>
 
             <a href="https://cesar-ctrl.github.io"className="card">
                 <style>{css}</style>
-                <img src={process.env.PUBLIC_URL + 'img/Dnd.PNG'} alt="home" />
+                <img src={process.env.PUBLIC_URL + '/img/Dnd.PNG'} alt="home" />
                 <div className="card-body">
                     <p className="card-text">Designed and developed a web guide about Dungeons and Dragons game, using HTML, CSS and
 JavaScript, among other technologies. This project helped me to improve my layout skills, as well as my
@@ -47,7 +47,7 @@ javascript knowledge. It also allowed me to learn how to consume REST web servic
             
             <a href="https://cesar-ctrl.github.io/proyecto2/"className="card">
                 
-                <img src={process.env.PUBLIC_URL + 'img/CrudTareas.PNG'} alt="home" />
+                <img src={process.env.PUBLIC_URL + '/img/CrudTareas.PNG'} alt="home" />
                 <div className="card-body">
                     <p className="card-text">Developed a CRUD of tasks that is connected to a database, everything that is not deleted stays in the database.
 I've done it with JavaScript, React and Bootstrap, it has state hooks.</p>
