1f2432a6885a14478f02529d939516d9dc5fc856
César <80521851+Cesar-ctrl@users.noreply.github.com>
2022-02-21 18:21:10 +0100

    Your awesome message

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Switch, Route,  Link } from "react-router-dom";
+import Inicio from "./Components/Inicio"
+import Contacto from "./Components/Contacto"
+import Work from "./Components/Work"
+import Nosotros from "./Components/Nosotros"
+import Usuarios from "./Components/Usuarios"
+import Formacion from "./Components/Formacion";
+import Footer from "./Components/Footer";
+
+function App() {
+    return (
+        
+        
+        <Router>
+            <div className="container mt-5">
+                
+                <div className="btn-group">
+                    <Link className="btn btn-datk btn-sm" to ="/">
+                        Inicio
+                    </Link>
+                    <Link className="btn btn-datk btn-sm" to ="/formacion">
+                        Formacion
+                    </Link>
+                    <Link className="btn btn-datk btn-sm" to ="/work">
+                        Work
+                    </Link>
+                    <Link className="btn btn-datk btn-sm" to ="/contacto">
+                        Contacto 
+                    </Link>
+                </div>
+
+                <hr/>
+                
+                <Routes>
+                
+                    <Route path="/contacto" element={<Contacto/>}/>
+                    <Route path="/formacion" element={<Formacion/>}/>
+                    <Route path="/work" element={<Work/>}/>
+                    
+                    <Route path="/" element={<Inicio/>}/>
+                    <Route path="/nosotros/:id" element={<Usuarios/>}/>
+                </Routes>
+            </div>
+            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            
+                <ul className='nav col-md-4 mb-0 '>
+                    <li className="nav-item"><a href="#" className="nav-link px-2">Email: amadocesar02@gmail.com</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 ">Phone: +34 601 314 076</a></li>
+                    <li className="nav-item"><a href="https://www.linkedin.com/in/césar-amado-096087232/" className="nav-link px-2">LinkedIn: https://www.linkedin.com/in/césar-amado-096087232/</a></li>
+                </ul> 
+        
+                
+                <ul className="nav col-md-4 justify-content-end">
+                    <li className="nav-item">
+                        <Link className="nav-link px-2 text-muted" to ="/">
+                            Inicio
+                        </Link></li>
+                    <li className="nav-item">
+                        <Link className="nav-link px-2 text-muted" to ="/formacion">
+                            Formacion
+                        </Link></li>
+                    <li className="nav-item">
+                        <Link className="nav-link px-2 text-muted" to ="/work">
+                            Work
+                        </Link></li>
+                    <li className="nav-item">
+                        <Link className="nav-link px-2 text-muted" to ="/contacto">
+                            Contacto 
+                        </Link></li>
+                </ul>
+                    
+            </footer>
+        </Router>
+        
+    );
+}
+
+export default App;
